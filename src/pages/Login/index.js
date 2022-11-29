aab8bc9ee5e032e2aa9cc279fa88e4f56925f615
Lucas <Lukas656>
2022-11-29 16:22:05 -0300

    Add pagina Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,12 +1,35 @@
 import Header from "../../component/Header"
+import './style.css'
 
-function Login(){
-    return(
+
+function Login() {
+    return (
         <>
-        <Header/>
-        <h1>Pagina Login</h1>
+            <Header />
+            <div className="main-login" >
+                <div className="left-login">
+                    <div className="left-login-image" alt="animação"></div>
+                </div>
+                <div className="right-login">
+                    <div className="card-login">
+                        <h1>Login</h1>
+                        <div className="textfield">
+                            <label for="usuario">Usuário</label>
+                            <input type="text" name="usuario" placeholder="Usuário" autoFocus required></input>
+                        </div>
+
+                        <div className="textfield">
+                            <label for="senha">Senha</label>
+                            <input type="password" name="senha" placeholder="Senha" required></input>
+                        </div>
+
+                        <p id="cc">Ainda não tem uma conta? <a href="#">Criar conta</a></p>
+                        <button className="btn-login">Entrar</button>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
 
-export default Login
+export default Login;
